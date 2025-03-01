3e65ff75f6a49634cd19b8d46dc1a2a93de9b565
Henry Zhao <l47zhao@uwaterloo.ca>
2025-03-01 00:22:19 -0500

    Created module 'integer' for integer-related operations.

---
+++ integer.c
@@ -0,0 +1,17 @@
+#include "integer.h"
+
+#include <assert.h>
+
+int numlen(int num) {
+  int len = (num < 0);
+  if (num < 0) {
+    num = -num;
+  }
+
+  do {
+    num /= 10;
+    len++;
+  } while (num != 0);
+
+  return len;
+}
---
+++ integer.h
@@ -0,0 +1,9 @@
+// "integer.h" provides additional utilities for integer operations.
+
+#ifndef INTEGER_H
+#define INTEGER_H
+
+// numlen(num) produces the length of num.
+int numlen(int num);
+
+#endif
