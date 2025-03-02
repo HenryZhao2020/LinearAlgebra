1ce51fa321643e46505892158ebed8109b0e4dea
Henry Zhao <henryzhao2019@gmail.com>
2025-03-01 23:21:17 -0500

    Completed implementations for computing the projection and perpendicular of vectors and planes

--- proj.c
+++ proj.c
@@ -2,16 +2,42 @@
 #include "vector.h"
 
 #include <assert.h>
-#include <stdio.h>
+
+// proj_assert(v1, v2) runs assertion tests to ensure that the projection
+//   and perpendicular of v1 onto v2 can be computed.
+void proj_assert(const struct vector *v1, const struct vector *v2) {
+  assert(is_vec_valid(v1));
+  assert(is_vec_valid(v2));
+  assert(v1->n == v2->n);
+  assert(!is_vec_zero(v2));
+}
 
 struct vector projv(const struct vector *u, const struct vector *v) {
-  assert(is_vec_valid(u));
-  assert(is_vec_valid(v));
-  assert(u->n == v->n);
-  assert(!is_vec_zero(v));
+  proj_assert(u, v);
 
   double len = vec_len(v);
   double coeff = (dot_product(u, v) / (len * len));
 
   return vec_scalar_mult(v, coeff);
+}
+
+struct vector perpv(const struct vector *u, const struct vector *v) {
+  proj_assert(u, v);
+
+  struct vector proj = projv(u, v);
+  struct vector neg_proj = vec_scalar_mult(&proj, -1);
+
+  return vec_add(u, &neg_proj);
+}
+
+struct vector projp(const struct vector *x, const struct vector *n) {
+  proj_assert(x, n);
+
+  return perpv(x, n);
+}
+
+struct vector perpp(const struct vector *x, const struct vector *n) {
+  proj_assert(x, n);
+
+  return projv(x, n);
 }
--- proj.h
+++ proj.h
@@ -11,4 +11,19 @@ struct vector;
 //           v is not a zero vector
 struct vector projv(const struct vector *u, const struct vector *v);
 
+// perpv(u, v) produces the perpendicular of u onto v.
+// requires: u and v are valid vectors in the same Rn
+//           v is not a zero vector
+struct vector perpv(const struct vector *u, const struct vector *v);
+
+// projp(x, n) produces the projection of x onto a plane with the normal vector n.
+// requires: x and n are valid vectors in the same Rn
+//           x is not a zero vector
+struct vector projp(const struct vector *x, const struct vector *n);
+
+// perpp(x, n) produces the perpendicular of x onto a plane with the normal vector n.
+// requires: x and n are valid vectors in the same Rn
+//           x is not a zero vector
+struct vector perpp(const struct vector *x, const struct vector *n);
+
 #endif
