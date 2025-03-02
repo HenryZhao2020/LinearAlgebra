36839d7fbe2ec0b83a1cac13789d54632e8dce16
Henry Zhao <henryzhao2019@gmail.com>
2025-03-01 22:40:19 -0500

    Renamed the module to 'real'

--- integer.h
+++
@@ -1,9 +0,0 @@
-// "integer.h" provides utilities for integer operations.
-
-#ifndef INTEGER_H
-#define INTEGER_H
-
-// numlen(num) produces the length of num, including the negative sign.
-int numlen(int num);
-
-#endif
