e06f9826a453d205d982a97393676dd65ff7b5ab
Henry Zhao <henryzhao2019@gmail.com>
2025-03-02 15:37:14 -0500

    Updated program interaction to include more details in computation and user prompts.
    Fixed issues with displaying -0 instead of 0.

--- main.c
+++ main.c
@@ -8,355 +8,408 @@
 #include <string.h>
 #include <assert.h>
 
-// All commands and their descriptions
-const char *cmd_table[] = {
-  "exit",     "Terminate the program",
-  "help",     "View program manual",
-  // ------------vector------------
-  "vadd",     "Perform addition of two vectors",
-  "vsmult",   "Perform scalar multiplication of a vector",
-  "dot",      "Compute the dot product of two vectors in Rn",
-  "cross",    "Compute the cross product of two vectors in R3",
-  "vlen",     "Determine the length (norm) of a vector",
-  "vang",     "Determine the angle between two vectors in radians",
-  // ----------proj, perp----------
-  "projv",    "Compute the projection of a vector onto another vector",
-  "perpv",    "Compute the perpendicular of a vector onto another vector",
-  "projp",    "Compute the projection of a vector in R3 onto a plane",
-  "perpp",    "Compute the perpendicular of a vector in R3 onto a plane",
-  // ------------matrix------------
-  "stdbas",   "Determine the standard basis of Rn",
-  
-  NULL,  // Indicate the end of the chart
-};
+// clear_screen() clears the console screen.
+void clear_screen() {
+  printf("\033[2J\033[H");
+}
 
-// read_vec_comp(vec, n, var) reads the components of vec in Rn.
-//   If the input is valid, the function updates vec and returns true.  
-//   Otherwise, vec remains unchanged, and the function returns false.  
-//   var is the name of vec.
-// requires: vec and var are not NULL
+// dash() prints a separating line with dashes.
+// effects: produces output
+void dash() {
+  const int dashes = 40;
+  for (int i = 0; i < dashes; ++i) {
+    printf("-");
+  }
+  printf("\n");
+}
+
+// read_vec_comp(v, n) prompts the components of v in Rn.
+//   The function updates v and produces true if all inputs were valid.
+//   Otherwise, v remains unchanged, and the function produces false.  
+// requires: v is not NULL
 //           0 <= n <= max_n
 // effects: reads input, produces output
-//          may mutate vec
-bool read_vec_comp(struct vector *vec, int n, const char *var) {
-  assert(vec);
+//          may mutate v
+bool read_vec_comp(struct vector *v, int n) {
+  assert(v && v->name);
   assert(n >= 0 && n <= max_n);
-  assert(var);
+
+  printf("Enter components of %s\n", v->name);
 
   double comp[100] = {0};
-  printf("Defining components of the vector %s\n", var);
   for (int i = 0; i < n; ++i) {
-    printf("x%d: ", (i + 1));
+    printf("%s%d: ", v->name, (i + 1));
     if (1 != scanf("%lf", &comp[i])) {
       return false;
     }
   }
 
   for (int i = 0; i < n; ++i) {
-    vec->comp[i] = comp[i];
+    // Avoid printing -0 instead of 0
+    if (comp[i] == -0.0) {
+      comp[i] = 0;
+    }
+    v->comp[i] = comp[i];
   }
 
   return true;
 }
 
-// read_vec(vec, var) reads the dimension and the components of a vector.  
-//   If the input is valid, the function updates vec and returns true.  
-//   Otherwise, vec remains unchanged, and the function returns false.  
+// read_vec(v_arr, num_v) prompts num_v vectors from v_arr.  
+//   The function updates the vectors in v_arr and produces true if 
+//   all inputs were valid.
+//   Otherwise, v remains unchanged, and the function produces false.  
 //   var is the name of vec.  
-// requires: vec and var are not NULL
+// requires: v_arr is not NULL
+//           num_v is the length of v_arr
 // effects: reads input, produces output
-//          may mutate vec
-bool read_vec(struct vector *vec, const char *var) {
-  assert(vec);
-  assert(var);
+//          may mutate v_arr
+bool read_vec(struct vector *v_arr[], int num_v) {
+  assert(v_arr);
+  assert(num_v > 0);
+  for (int i = 0; i < num_v; ++i) {
+    assert(v_arr[i] && v_arr[i]->name);
+  }
 
   int n = 0;
-  printf("Defining vector %s\n", var);
-  printf("Dimension: ");
+  printf("Enter the vector dimension (n): ");  
   if (1 != scanf("%d", &n)) {
     return false;
   }
+  printf("\n");
 
-  if (!read_vec_comp(vec, n, var)) {
-    return false;
+  for (int i = 0; i < num_v; ++i) {
+    if (!read_vec_comp(v_arr[i], n)) {
+      return false;
+    }
+    printf("\n");
   }
-  vec->n = n;
+
+  for (int i = 0; i < num_v; ++i) {
+    v_arr[i]->n = n;
+  }
 
   return true;
 }
 
 // exec_vadd() performs addition of two vectors.
-//   The function produces true if the operation is successful and false otherwise.
+//   The function produces true if the operations succeed and false otherwise.
 // effects: reads input, produces output
 bool exec_vadd() {
-  struct vector v1 = {0, {0}};
-  struct vector v2 = {0, {0}};
+  struct vector u = {0, {0}, "u"};
+  struct vector v = {0, {0}, "v"};
 
-  if (!read_vec(&v1, "v1")) {
+  printf("Let %s and %s be vectors in Rn.\n", u.name, v.name);
+  printf("We want to evaluate %s + %s\n\n", u.name, v.name);  
+
+  struct vector *v_arr[] = {&u, &v};
+  if (!read_vec(v_arr, 2)) {
     return false;
   }
-  printf("\n");
-  if (!read_vec(&v2, "v2")) {
-    return false;
-  }
-  printf("\n");
 
-  struct vector sum = vec_add(&v1, &v2);
-  printf("Vector addition:\n");
-  print_vec(&v1);
-  printf("+\n");
-  print_vec(&v2);
-  printf("=\n");
-  print_vec(&sum);
-  printf("\n");
+  struct vector sum = vec_add(&u, &v);
+  printf("Adding each component of %s and %s:\n", u.name, v.name);
+  for (int i = 0; i < v.n; ++i) {
+    printf("\t%s%d + %s%d = %g\n", 
+           u.name, (i + 1), v.name, (i + 1), sum.comp[i]);
+  }
+
+  printf("\nTherefore, %s + %s =\n", u.name, v.name);
+  print_vec(&sum, true);
 
   return true;
 }
 
 // exec_vsmult() performs scalar multiplication of a vector.
-//   The function produces true if the operation is successful and false otherwise.
+//   The function produces true if the operations succeed and false otherwise.
 // effects: reads input, produces output
 bool exec_vsmult() {
-  struct vector vec = {0, {0}};
-  if (!read_vec(&vec, "v")) {
+  struct vector v = {0, {0}, "v"};
+
+  printf("Let %s be a vector in Rn.\n", v.name);
+  printf("Let c be a real number.\n");
+  printf("We want to evaluate c%s\n\n", v.name);
+
+  struct vector *v_arr[] = {&v};
+  if (!read_vec(v_arr, 1)) {
     return false;
   }
-  printf("\n");
 
   double c = 0;
-  printf("c: ");
+  printf("Enter the value of c: ");
   if (1 != scanf("%lf", &c)) {
     return false;
   }
   printf("\n");
 
-  struct vector prod = vec_scalar_mult(&vec, c);
-  printf("Vector scalar multiplication:\n");
-  printf("%g\n", c);
-  print_vec(&vec);
-  printf("=\n");
-  print_vec(&prod);
-  printf("\n");
-
+  struct vector prod = vec_scalar_mult(&v, c);
+  printf("Multiplying each component of %s by c:\n", v.name);
+  for (int i = 0; i < v.n; ++i) {
+    printf("\tc(%s%d) = %g\n", v.name, (i + 1), prod.comp[i]);
+  }
+  
+  printf("\nTherefore, c%s =\n", v.name);
+  print_vec(&prod, true);
+  
   return true;
 }
 
-// exec_dot() computes the dot product of two vectors in Rn.
-//   The function produces true if the operation is successful and false otherwise.
+// exec_dot() evaluates the dot product of two vectors in Rn.
+//   The function produces true if the operations succeed and false otherwise.
 // effects: reads input, produces output
 bool exec_dot() {
-  struct vector v1 = {0, {0}};
-  struct vector v2 = {0, {0}};
+  struct vector u = {0, {0}, "u"};
+  struct vector v = {0, {0}, "v"};
 
-  if (!read_vec(&v1, "v1")) {
+  printf("Let %s and %s be vectors in Rn.\n", u.name, v.name);
+  printf("We want to evaluate the dot product %s · %s\n\n", u.name, v.name);  
+
+  struct vector *v_arr[] = {&u, &v};
+  if (!read_vec(v_arr, 2)) {
     return false;
   }
-  printf("\n");
-  if (!read_vec(&v2, "v2")) {
-    return false;
-  }
-  printf("\n");
 
-  int dp = dot_product(&v1, &v2);
-  printf("Dot product of v1 and v2 is: %d\n", dp);
+  double dot = dot_product(&u, &v);
+  for (int i = 0; i < v.n; ++i) {
+    if (i) {
+      printf(" + ");
+    }
+    printf("%s%d%s%d", u.name, (i + 1), v.name, (i + 1));
+  }
+  printf(" = %g\n\n", dot);
+  
+  printf("Therefore, the dot product %s · %s is %g\n", 
+         u.name, v.name, dot);
 
   return true;
 }
 
-// exec_cross() computes the cross product of two vectors in R3.
-//   The function produces true if the operation is successful and false otherwise.
+// exec_cross() evaluates the cross product of two vectors in R3.
+//   The function produces true if the operations succeed and false otherwise.
 // effects: reads input, produces output
 bool exec_cross() {
-  struct vector v1 = {3, {0}};
-  struct vector v2 = {3, {0}};
+  struct vector u = {3, {0}, "u"};
+  struct vector v = {3, {0}, "v"};
 
-  if (!read_vec_comp(&v1, 3, "v1 in R3")) {
-    return false;
-  }
+  printf("Let %s and %s be vectors in R3.\n", u.name, v.name);
+  printf("We want to evaluate %s x %s\n\n", u.name, v.name);  
+
+  read_vec_comp(&u, 3);
   printf("\n");
-  if (!read_vec_comp(&v2, 3, "v2 in R3")) {
-    return false;
-  }
+  read_vec_comp(&v, 3);
   printf("\n");
 
-  struct vector cross = cross_product(&v1, &v2);
-  printf("Cross product of v1 and v2 is:\n");
-  print_vec(&cross);
+  printf("Evaluating components of the cross product:\n");
+  struct vector cross = cross_product(&u, &v);
+  const int indices[] = {2, 3, 3, 1, 1, 2};
+  for (int i = 0; i < 3; ++i) {
+    printf("\t%s%d%s%d - %s%d%s%d = %g\n", 
+           u.name, indices[i * 2], v.name, indices[i * 2 + 1], 
+           u.name, indices[i * 2 + 1], v.name, indices[i * 2], 
+           cross.comp[i]);
+  }
+
+  printf("\nTherefore, the cross product %s x %s is:\n", u.name, v.name);
+  print_vec(&cross, true);
 
   return true;
 }
 
 // exec_vlen() determines the length (norm) of a vector.
-//   The function produces true if the operation is successful and false otherwise.
+//   The function produces true if the operations succeed and false otherwise.
 // effects: reads input, produces output
 bool exec_vlen() {
-  struct vector vec = {0, {0}};
-  if (!read_vec(&vec, "v")) {
+  struct vector v = {0, {0}, "v"};
+
+  printf("Let %s be a vector in Rn.\n", v.name);
+  printf("We want to determine the length (norm) of v.\n\n");
+
+  struct vector *v_arr[] = {&v};
+  if (!read_vec(v_arr, 1)) {
     return false;
   }
-  printf("\n");
-
-  double len = vec_len(&vec);
-  printf("Length (norm) of v is: %g\n", len);
+  
+  double len = vec_len(&v);
+  printf("||%s|| = √(%s · %s) = %g\n\n", v.name, v.name, v.name, len);
+  printf("Therefore, the length (norm) of %s is: %g\n", v.name, len);
 
   return true;
 }
 
 // exec_vang() determines the angle between two vectors in radians.
-//   The function produces true if the operation is successful and false otherwise.
+//   The function produces true if the operations succeed and false otherwise.
 // effects: reads input, produces output
 bool exec_vang() {
-  struct vector v1 = {0, {0}};
-  struct vector v2 = {0, {0}};
+  struct vector u = {0, {0}, "u"};
+  struct vector v = {0, {0}, "v"};
 
-  if (!read_vec(&v1, "v1")) {
+  printf("Let %s and %s be vectors in Rn.\n", u.name, v.name);
+  printf("We want to determine the angle (θ) between %s and %s, "
+         "where θ is in radians.\n\n", u.name, v.name);  
+
+  struct vector *v_arr[] = {&u, &v};
+  if (!read_vec(v_arr, 2)) {
     return false;
   }
-  printf("\n");
-  if (!read_vec(&v2, "v2")) {
-    return false;
-  }
-  printf("\n");
 
-  double ang = vec_angle(&v1, &v2);
-  printf("Angle between v1 and v2 in radians is: %g\n", ang);
+  double ang = vec_angle(&u, &v);
+  printf("θ = arccos((%s · %s) / (||%s|| ||%s||)) = %g\n\n", 
+         u.name, v.name, u.name, v.name, ang);
+
+  printf("Therefore, the angle (θ) between %s and %s in radians is %g\n", 
+         u.name, v.name, ang);
 
   return true;
 }
 
-// exec_stdbas() determines the standard basis for Rn.
-//   The function produces true if the operation is successful and false otherwise.
+// exec_projv() finds the projection of a vector onto another vector.
 // effects: reads input, produces output
-bool exec_stdbas() {
-  int n = 0;
-  printf("Dimension: ");
-  if (1 != scanf("%d", &n)) {
-    return false;
-  }
-
-  struct matrix basis = std_basis(n);
-  printf("Standard basis for R%d\n", n);
-  print_matrix(&basis);
-
-  return true;
-}
-
 bool exec_projv() {
-  struct vector u = {0, {0}};
-  struct vector v = {0, {0}};
+  struct vector u = {0, {0}, "u"};
+  struct vector v = {0, {0}, "v"};
 
-  if (!read_vec(&u, "u")) {
+  printf("Let %s and %s be vectors in Rn.\n", u.name, v.name);
+  printf("We want to find the projection of %s onto %s.\n\n",
+         u.name, v.name);
+
+  struct vector *v_arr[] = {&u, &v};
+  if (!read_vec(v_arr, 2)) {
     return false;
   }
-  printf("\n");
-  if (!read_vec(&v, "v")) {
-    return false;
-  }
-  printf("\n");
+
+  printf("proj%s(%s) = [(%s · %s) / ||%s||²] %s\n\n", 
+         v.name, u.name, u.name, v.name, v.name, v.name);
 
   struct vector proj = projv(&u, &v);
-  printf("The projection of u onto v is:\n");
-  print_vec(&proj);
+  printf("Therefore, the projection of %s onto %s is:\n", u.name, v.name);
+  print_vec(&proj, true);
 
   return true;
 }
 
+// exec_perpv() finds the perpendicular of a vector onto another vector.
+// effects: reads input, produces output
 bool exec_perpv() {
-  struct vector u = {0, {0}};
-  struct vector v = {0, {0}};
+  struct vector u = {0, {0}, "u"};
+  struct vector v = {0, {0}, "v"};
 
-  if (!read_vec(&u, "u")) {
+  printf("Let %s and %s be vectors in Rn.\n", u.name, v.name);
+  printf("We want to find the perpendicular of %s onto %s.\n\n",
+         u.name, v.name);
+
+  struct vector *v_arr[] = {&u, &v};
+  if (!read_vec(v_arr, 2)) {
     return false;
   }
-  printf("\n");
-  if (!read_vec(&v, "v")) {
-    return false;
-  }
-  printf("\n");
 
-  struct vector proj = perpv(&u, &v);
-  printf("The perpendicular of u onto v is:\n");
-  print_vec(&proj);
+  printf("perp%s(%s) = %s - [(%s · %s) / ||%s||²] %s\n\n", 
+         v.name, u.name, u.name, u.name, v.name, v.name, v.name); 
+
+  struct vector perp = perpv(&u, &v);
+  printf("Therefore, the perpendicular of %s onto %s is:\n", u.name, v.name);
+  print_vec(&perp, true);
 
   return true;
 }
 
+// exec_projp() finds the projection of a vector in R3 onto a plane.
+// effects: reads input, produces output
 bool exec_projp() {
-  struct vector x = {3, {0}};
-  struct vector n = {3, {0}};
+  struct vector x = {3, {0}, "x"};
+  struct vector n = {3, {0}, "n"};
 
-  if (!read_vec_comp(&x, 3, "x in R3")) {
-    return false;
-  }
+  printf("Let %s and %s be vectors in R3.\n", x.name, n.name);
+  printf("We want to find the projection of %s onto a plane,\n", x.name);
+  printf("where the plane has a normal vector %s.\n\n", n.name);
+
+  read_vec_comp(&x, 3);
   printf("\n");
-  if (!read_vec_comp(&n, 3, "n in R3")) {
-    return false;
-  }
+  read_vec_comp(&n, 3);
   printf("\n");
+
+  printf("projP(%s) = %s - [(%s · %s) / ||%s||²] %s\n\n", 
+         x.name, x.name, x.name, n.name, n.name, n.name);
 
   struct vector proj = projp(&x, &n);
-  printf("The projection of x onto the plane (normal vector = n) is:\n");
-  print_vec(&proj);
+  printf("The projection of x onto the plane is:\n");
+  print_vec(&proj, true);
 
   return true;
 }
 
+// exec_perpp() finds the perpendicular of a vector in R3 onto a plane.
+// effects: reads input, produces output
 bool exec_perpp() {
-  struct vector x = {3, {0}};
-  struct vector n = {3, {0}};
+  struct vector x = {3, {0}, "x"};
+  struct vector n = {3, {0}, "n"};
 
-  if (!read_vec_comp(&x, 3, "x in R3")) {
-    return false;
-  }
+  printf("Let %s and %s be vectors in R3.\n", x.name, n.name);
+  printf("We want to find the perpendicular of %s onto a plane,\n", x.name);
+  printf("where the plane has a normal vector %s.\n\n", n.name);
+
+  read_vec_comp(&x, 3);
   printf("\n");
-  if (!read_vec_comp(&n, 3, "n in R3")) {
-    return false;
-  }
+  read_vec_comp(&n, 3);
   printf("\n");
+
+  printf("perpP(%s) = [(%s · %s) / ||%s||²] %s\n\n", 
+         x.name, x.name, n.name, n.name, n.name);
 
   struct vector perp = perpp(&x, &n);
-  printf("The perpendicular of x onto the plane (normal vector = n) is:\n");
-  print_vec(&perp);
+  printf("The perpendicular of x onto the plane is:\n");
+  print_vec(&perp, true);
 
   return true;
 }
 
-// help() outputs program mannual and developer info.
+// help() outputs program manual and developer info.
 // effects: produces output
 void help() {
-  const int dashes = 35;
-
-  printf("Command\t\tOperation\n");
-  for (int i = 0; i < dashes; ++i) {
-    printf("-");
-  }
+  printf("Program Controls\n");
+  dash();
+  printf("exit\t\tTerminate the program\n");
+  printf("help\t\tView program manual\n");
   printf("\n");
 
-  const char **table = cmd_table;
-  while (*table) {
-    printf("%s\t\t%s\n", *table, *(table + 1));
-    table += 2;
-  }
-
-  printf("\nProgram Information\n");
-  for (int i = 0; i < dashes; ++i) {
-    printf("-");
-  }
+  printf("Vector Operations\n");
+  dash();
+  printf("vadd\t\tPerform addition of two vectors\n");
+  printf("vsmult\t\tPerform scalar multiplication of a vector\n");
+  printf("dot\t\tEvaluate the dot product of two vectors\n");
+  printf("cross\t\tEvaluate the cross product of two vectors in R3\n");
+  printf("vlen\t\tDetermine the length (norm) of a vector\n");
+  printf("vang\t\tDetermine the angle between two vectors in radians\n");
   printf("\n");
-  printf("Developer:\tHenry Zhao\n");
-  printf("Version:\tv1.0\n");
-  printf("Reference:\tD. Wolczuk, MATH 136 Course Notes Edition 2024.1\n");
+
+  printf("Projections\n");
+  dash();
+  printf("projv\t\tFind the projection of a vector onto another vector\n");
+  printf("perpv\t\tFind the perpendicular of a vector onto another vector\n");
+  printf("projp\t\tFind the projection of a vector in R3 onto a plane\n");
+  printf("perpp\t\tFind the perpendicular of a vector in R3 onto a plane\n");
+  printf("\n");
+
+//   printf("Matrix Operations\n");
+//   dash();
+//   printf("madd\t\tPerform addition of two matrices\n");
+//   printf("msmult\t\tPerform scalar multiplication of a matrix\n");
+//   printf("\n");
+
+//   printf("Basis\n");
+//   dash();
+//   printf("stdbas\t\tDetermine the basis for Rn\n");
 }
 
 int main(void) {
   const int cmd_max_len = 10;
 
+  clear_screen();
   printf("Welcome to the Centre for Linear Algebra!\n");
   printf("Please enter 'help' to get started.\n\n\n");
   
   while (true) {
     char cmd[11];
-    printf("Enter command (e.g. va, dot): ");
+    printf("Enter command (e.g. vadd, dot): ");
     if (1 != scanf("%10s", cmd)) {
       break;
     }
@@ -386,14 +439,12 @@ int main(void) {
       exec_projp();
     } else if (!strcmp(cmd, "perpp")) {
       exec_perpp();
-    } else if (!strcmp(cmd, "stdbas")) {
-      exec_stdbas();
     } else {
       printf("Invalid command! Please enter 'help' to see the program manual.\n");
     }
 
-    printf("\n");
+    printf("\n\n");
   }
 
-  printf("\nHope you enjoy linear algebra! See you next time.\n");
+  printf("Hope you enjoy linear algebra! See you next time.\n");
 }
